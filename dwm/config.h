b9ef68594923928b17ef3eb35145ad6bfa5dd29c
tkuwill <billwang1023@gmail.com>
2022-07-07 17:36:03 +0800

    Added the patch of dwm-alternativetags

--- dwm/config.h
+++ dwm/config.h
@@ -1,12 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 10;        /* border pixel of windows */
+static const unsigned int borderpx  = 8;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "SauceCodePro Nerd Font Medium:size=12" };
-static const char dmenufont[]       = "SauceCodePro Nerd Font Medium:size=10";
+static const char *fonts[]          = { "SauceCodeProNerdFont:size=12" };
+static const char dmenufont[]       = "SauceCodeProNerdFont:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -19,7 +19,9 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tagsalt[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "", "", "輸", "", "" };
+static const int momentaryalttags = 0; /* 1 means alttags will show only when key is held down*/
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -27,7 +29,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{"Gimp", NULL, NULL, 0, 1, -1 },
+	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{"pavucontrol", NULL, NULL, 0, 1, -1},
 	{"copyq", NULL, NULL, 0, 1, -1},
 	{"gnome-calculator", NULL, NULL, 0, 1, -1},
@@ -37,7 +39,7 @@ static const Rule rules[] = {
 	{"blueman-manager", NULL, NULL, 0, 1, -1},
 	{"nm-connection-editor", NULL, NULL, 0, 1, -1},
 	{"fcitx5", NULL, NULL, 0, 1, -1},
-	{"Firefox", NULL, NULL, 1 << 8, 0, -1},
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
@@ -69,6 +71,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *rofidruncmd[] = {"rofi", "-show", "drun", NULL};
+
 static Key keys[] = {
 	/* modifier                     key        function        argument */
     { MODKEY,                       XK_s,      spawn,          {.v = rofidruncmd } },
@@ -95,6 +98,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_n,      togglealttag,   {0} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
