c68543c8d3f3880bb0ee405f8a7e193333fadc13
tkuwill <billwang1023@gmail.com>
2022-07-07 13:10:48 +0800

    Add the command of starting rofi in dwm

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -68,9 +68,10 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-
+static const char *rofidruncmd[] = {"rofi", "-show", "drun", NULL};
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+    { MODKEY,                       XK_s,      spawn,          {.v = rofidruncmd } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
