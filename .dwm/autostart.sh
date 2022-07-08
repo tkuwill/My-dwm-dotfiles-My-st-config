bd8c9d8da5e17278e99e290ca37e300af5120f72
tkuwill <billwang1023@gmail.com>
2022-07-08 12:48:51 +0800

    Added the volume module

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -9,8 +9,9 @@ feh --bg-fill /home/will/Pictures/DesktopBackground/wallpaperdwm.jpg &
 
 # xsetroot for dwm
 dwm_date () {
-  date '+%Y年%m月%d日 %a %H:%M'
+    date '+%Y年%m月%d日(%a)%H:%M'
 }
+# by joestandring/dwm-bar
 
 dwm_battery () {
     # Change BAT1 to whatever your battery is identified as. Typically BAT0 or BAT1
@@ -34,8 +35,42 @@ bat_time () {
   acpi | grep 'Battery 0' | grep  -Eo '[0-9][0-9]:[0-9][0-9]'
 }
 
+dwm_alsa () {
+	STATUS=$(amixer sget Master | tail -n1 | sed -r "s/.*\[(.*)\]/\1/")
+    VOL=$(amixer get Master | tail -n1 | sed -r "s/.*\[(.*)%\].*/\1/")
+    printf "%s" "$SEP1"
+    if [ "$IDENTIFIER" = "unicode" ]; then
+    	if [ "$STATUS" = "off" ]; then
+	            printf "🔇"
+    	else
+    		#removed this line becuase it may get confusing
+	        if [ "$VOL" -gt 0 ] && [ "$VOL" -le 33 ]; then
+	            printf "🔈 %s%%" "$VOL"
+	        elif [ "$VOL" -gt 33 ] && [ "$VOL" -le 66 ]; then
+	            printf "🔉 %s%%" "$VOL"
+	        else
+	            printf "🔊 %s%%" "$VOL"
+	        fi
+		fi
+    else
+    	if [ "$STATUS" = "off" ]; then
+    		printf "MUTE"
+    	else
+	        # removed this line because it may get confusing
+	        if [ "$VOL" -gt 0 ] && [ "$VOL" -le 33 ]; then
+	            printf "VOL %s%%" "$VOL"
+	        elif [ "$VOL" -gt 33 ] && [ "$VOL" -le 66 ]; then
+	            printf "VOL %s%%" "$VOL"
+	        else
+	            printf "VOL %s%%" "$VOL"
+        	fi
+        fi
+    fi
+    printf "%s\n" "$SEP2"
+}
+
 while true
 do
-    xsetroot -name "$(dwm_date) | $(dwm_battery) | $(bat_time)-remaining"
+    xsetroot -name "$(dwm_alsa)|$(dwm_date)|$(dwm_battery)|$(bat_time)-remaining"
     sleep 1 
 done
