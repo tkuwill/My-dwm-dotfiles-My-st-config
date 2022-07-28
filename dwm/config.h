ea3854e6445be36c2018274f5b99c726b2b7b790
tkuwill <billwang1023@gmail.com>
2022-07-28 16:32:30 +0800

    Update dmenu's font config

--- dwm/config.h
+++ dwm/config.h
@@ -6,7 +6,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "SauceCodeProNerdFont:size=12" };
-static const char dmenufont[]       = "SauceCodeProNerdFont:size=10";
+static const char dmenufont[]       = "SauceCodeProNerdFont:size=15";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
