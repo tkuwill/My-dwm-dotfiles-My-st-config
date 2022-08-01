4fd73a029957d9ababbcd8ab0aec4183fb541d48
tkuwill <billwang1023@gmail.com>
2022-08-01 17:20:12 +0800

    Update dmenu's config

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -74,7 +74,8 @@ static const Layout layouts[] = {
 #include <X11/XF86keysym.h>
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+/* static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL }; */
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *rofidruncmd[] = {"rofi", "-show", "drun", "-config", "/home/will/.config/rofi/Paper.rasi", NULL};
 
