3ddf6ba786c2cd50636cc58a6e266ce8a9bca371
tkuwill <billwang1023@gmail.com>
2022-08-04 20:26:45 +0800

    Added cpu_fan and cpu_temp info

--- .scripts/dmenu/sysinfo.sh
+++ .scripts/dmenu/sysinfo.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # ram.png is from <a href="https://www.flaticon.com/free-icons/ram" title="ram icons">Ram icons created by Freepik - Flaticon</a>.
-
+# You have to install lm_sensors, acpi, libnotify, and a notification package (like dunst) in order to use this script.
 
 function mem {
     free -h | grep 'Mem' | cut -c 28-32
@@ -14,13 +14,25 @@ function battime {
     acpi | grep 'Battery 0' | grep  -Eo '[0-9][0-9]:[0-9][0-9]'
 }
 
+function cpu_fan {
+    sensors | grep 'cpu_fan'
+}
+
+function cpu_temp {
+    sensors | grep 'Core 0' | cut -c 16-23
+}
+
 function sysinfo {
-    options="Cancel\nMemory\nBAT-remaining"
+    options="Cancel\nMemory\nBAT-remaining\ncpu_fan\ncpu_temp"
     selected=$(echo -e $options | dmenu -i -p "System info")
     if [[ $selected = "Memory" ]]; then 
-        notify-send -t 8000 "Mem is used : $(mem)/$(allmem)." 
+        notify-send -t 8000 "Mem is used : $(mem)  /  $(allmem)." 
     elif [[ $selected = "BAT-remaining" ]]; then 
-        notify-send -t 8000 "BAT-time : $(battime)-remaining."  
+        notify-send -t 8000 "BAT-time : $(battime) - remaining."  
+    elif [[ $selected = "cpu_fan" ]]; then 
+        notify-send -t 5000 "$(cpu_fan)"
+    elif [[ $selected = "cpu_temp" ]]; then 
+        notify-send -t 5000 "CPU temp: $(cpu_temp) ."
     elif [[ $selected = "Cancel" ]]; then 
         return
     fi
