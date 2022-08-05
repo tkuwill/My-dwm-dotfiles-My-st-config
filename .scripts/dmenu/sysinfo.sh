43c3553646b0ff57e8a6a76f581b5cb65867449a
tkuwill <billwang1023@gmail.com>
2022-08-05 19:39:38 +0800

    Added "Now playing" into scripts and add the icons

--- .scripts/dmenu/sysinfo.sh
+++ .scripts/dmenu/sysinfo.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 # ram.png is from <a href="https://www.flaticon.com/free-icons/ram" title="ram icons">Ram icons created by Freepik - Flaticon</a>.
-# You have to install lm_sensors, acpi, libnotify, and a notification package (like dunst) in order to use this script.
+# music.png is from <a href="https://www.flaticon.com/free-icons/google-play-music" title="google play music icons">Google play music icons created by Mayor Icons - Flaticon</a>
+# battery.png is from <a href="https://www.flaticon.com/free-icons/full-battery" title="full battery icons">Full battery icons created by Pixel perfect - Flaticon</a>.
+# cpu.png is from <a href="https://www.flaticon.com/free-icons/thermometer" title="thermometer icons">Thermometer icons created by Smashicons - Flaticon</a>.
+# fan.png is from <a href="https://www.flaticon.com/free-icons/fan" title="fan icons">Fan icons created by Nikita Golubev - Flaticon</a>.
+# You have to install lm_sensors, acpi, libnotify, playerctl and a notification package (like dunst) in order to use this script.
 
 function mem {
     free -h | grep 'Mem' | cut -c 28-32
@@ -22,17 +26,25 @@ function cpu_temp {
     sensors | grep 'Core 0' | cut -c 16-23
 }
 
+function now_play {
+    playerctl metadata --format "{{ title }} 
+{{ artist }} - {{ album }}"
+}
+
+
 function sysinfo {
-    options="Cancel\nMemory\nBAT-remaining\ncpu_fan\ncpu_temp"
+    options="Cancel\nMemory\nBAT-remaining\ncpu_fan\ncpu_temp\nNow_playing"
     selected=$(echo -e $options | dmenu -i -p "System info")
     if [[ $selected = "Memory" ]]; then 
-        notify-send -t 8000 "Mem is used : $(mem)  /  $(allmem)." 
+        notify-send -i /home/will/Pictures/sysicon/ram.png -t 8000 "Mem is used : $(mem)  /  $(allmem)." 
     elif [[ $selected = "BAT-remaining" ]]; then 
-        notify-send -t 8000 "BAT-time : $(battime) - remaining."  
+        notify-send -i /home/will/Pictures/sysicon/battery.png -t 8000 "BAT-time : $(battime) - remaining."  
     elif [[ $selected = "cpu_fan" ]]; then 
-        notify-send -t 5000 "$(cpu_fan)"
+        notify-send -i /home/will/Pictures/sysicon/fan.png -t 5000 "$(cpu_fan)"
     elif [[ $selected = "cpu_temp" ]]; then 
-        notify-send -t 5000 "CPU temp: $(cpu_temp) ."
+        notify-send -i /home/will/Pictures/sysicon/cpu.png -t 5000 "CPU temp: $(cpu_temp) ."
+    elif [[ $selected = "Now_playing" ]]; then 
+        notify-send -i /home/will/Pictures/sysicon/music.png -t 5000 "$(now_play)"
     elif [[ $selected = "Cancel" ]]; then 
         return
     fi
