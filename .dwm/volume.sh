e2703a2b8dcac337e0a017e40b812eb9561a9665
tkuwill <billwang1023@gmail.com>
2022-08-05 20:34:53 +0800

    Change the icons of volume up and volume down

--- .dwm/volume.sh
+++ .dwm/volume.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-# volume up, down, mute icon are from <a href="https://www.flaticon.com/free-icons/volume" title="volume icons">Volume icons created by nawicon - Flaticon</a>.
+# volume up icon is from <a href="https://www.flaticon.com/free-icons/volume-up" title="volume up icons">Volume up icons created by Freepik - Flaticon</a>.
+# volume down icon is from "https://www.flaticon.com/free-icons/volume-down" title="volume down icons">Volume down icons created by Freepik - Flaticon
+# volume mute icon is from <a href="https://www.flaticon.com/free-icons/ui" title="ui icons">Ui icons created by Icon Hubs - Flaticon</a>.
 # Credit from https://gist.github.com/sebastiencs/5d7227f388d93374cebdf72e783fbd6a & https://wiki.archlinux.org/title/Dunst  .
 
 # You can call this script like this:
@@ -23,14 +25,29 @@ function send_notification {
     # Send the notification
     if [[ $volume == 0 || "$mute" == "off" ]]; then
     # Show the sound muted notification
-    dunstify -t 1000 -a "changeVolume" -i /home/will/Pictures/sysicon/volume-mute.png  -h string:x-dunst-stack-tag:$msgTag "Volume muted" 
+    dunstify -t 1000 -a "changeVolume" -i /home/will/Pictures/sysicon/mute.png  -h string:x-dunst-stack-tag:$msgTag "Volume muted" 
 else
     # Show the volume notification
-    dunstify -t 1000 -a "changeVolume" -i /home/will/Pictures/sysicon/volume.png -h string:x-dunst-stack-tag:$msgTag \
+    dunstify -t 1000 -a "changeVolume" -i /home/will/Pictures/sysicon/volume-up.png -h string:x-dunst-stack-tag:$msgTag \
     -h int:value:"$volume" "Volume: ${volume}%"
 fi
 }
 
+function send_notification1 {
+    volume=`get_volume`
+    # Make the bar with the special character ─ (it's not dash -)
+    # https://en.wikipedia.org/wiki/Box-drawing_character
+    bar=$(seq -s "─" $(($volume / 5)) | sed 's/[0-9]//g')
+    # Send the notification
+    if [[ $volume == 0 || "$mute" == "off" ]]; then
+    # Show the sound muted notification
+    dunstify -t 1000 -a "changeVolume" -i /home/will/Pictures/sysicon/mute.png  -h string:x-dunst-stack-tag:$msgTag "Volume muted" 
+else
+    # Show the volume notification
+    dunstify -t 1000 -a "changeVolume" -i /home/will/Pictures/sysicon/volume-down.png -h string:x-dunst-stack-tag:$msgTag \
+    -h int:value:"$volume" "Volume: ${volume}%"
+fi
+}
 case $1 in
     up)
 	# Set the volume on (if it was muted)
@@ -42,13 +59,13 @@ case $1 in
     down)
 	amixer -D pulse set Master on > /dev/null
 	amixer -D pulse sset Master 3%- > /dev/null
-	send_notification
+	send_notification1
 	;;
     mute)
     	# Toggle mute
 	amixer -D pulse set Master 1+ toggle > /dev/null
 	if is_mute ; then
-        dunstify -t 1000 -a "changeVolume" -i /home/will/Pictures/sysicon/volume-mute.png  -h string:x-dunst-stack-tag:$msgTag "Volume muted"
+        dunstify -t 1000 -a "changeVolume" -i /home/will/Pictures/sysicon/mute.png  -h string:x-dunst-stack-tag:$msgTag "Volume muted"
 
 	fi
 	;;
