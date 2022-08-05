9c378ef06687b8735ca994a08be549dcfbf68004
tkuwill <billwang1023@gmail.com>
2022-08-05 20:35:21 +0800

    Change the icon of Normal mode

--- .scripts/dmenu/donotdisturb.sh
+++ .scripts/dmenu/donotdisturb.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # bell.png is from <a href="https://www.flaticon.com/free-icons/ui" title="ui icons">Ui icons created by apien - Flaticon</a>
-# notification.png is from https://www.flaticon.com/premium-icon/notification_3106685
+# bell-ring.png is from <a href="https://www.flaticon.com/free-icons/notification" title="notification icons">Notification icons created by Freepik - Flaticon</a>.
 function notification {
     options="Cancel\nDo not disturb\nNormal"
     selected=$(echo -e $options | dmenu -i -p "Notification Mode")
     if [[ $selected = "Do not disturb" ]]; then 
         notify-send -i /home/will/Pictures/sysicon/bell.png -u critical -t 3000 "Do Not Disturb is now ON." && sleep 5s && dunstctl set-paused true
     elif [[ $selected = "Normal" ]]; then 
-        dunstctl set-paused false && notify-send -i /home/will/Pictures/sysicon/notification.png -u critical -t 8000 "Do Not Disturb is now OFF."  
+        dunstctl set-paused false && notify-send -i /home/will/Pictures/sysicon/bell-ring.png -u critical -t 8000 "Do Not Disturb is now OFF."  
     elif [[ $selected = "Cancel" ]]; then 
         return
     fi
