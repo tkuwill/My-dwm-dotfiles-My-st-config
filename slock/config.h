53a2eaa202bf979f616f042ecb6c4dc8588122f0
tkuwill <billwang1023@gmail.com>
2022-10-02 16:42:17 +0800

    Add my config of slock

---
+++ slock/config.h
@@ -0,0 +1,22 @@
+/* user and group to drop privileges to */
+static const char *user  = "nobody";
+static const char *group = "nobody";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "black",     /* after initialization */
+	[INPUT] =  "#005577",   /* during input */
+	[FAILED] = "#CC3333",   /* wrong password */
+	[CAPS] = "green",         /* CapsLock on */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
+
+/* default message */
+static const char * message = "Suckless: Software that sucks less.";
+
+/* text color */
+static const char * text_color = "#ffffff";
+
+/* text size (must be a valid size) */
+static const char * font_name = "-misc-dejavu serif-bold-r-normal--0-0-0-0-p-0-iso8859-16";
